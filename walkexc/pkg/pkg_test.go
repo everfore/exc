2056839d7e8c67d371f6359f26e1065ec52982dd
shiyongbin <shiyongbin@ezbuy.com>
2017-12-09 14:17:35 +0800

    pkg_cmd

---
+++ walkexc/pkg/pkg_test.go
@@ -0,0 +1,18 @@
+package pkg
+
+// import (
+// 	"fmt"
+// 	"testing"
+
+// 	"github.com/everfore/exc"
+// 	"github.com/toukii/goutils"
+// )
+
+// func TestPkg(t *testing.T) {
+// 	bs, err := exc.NewCMD("go list -json").DoNoTime()
+// 	pkgInfo, err := goutils.NewPkg(bs)
+// 	if goutils.CheckErr(err) {
+// 		return
+// 	}
+// 	fmt.Println(goutils.DisplayString(pkgInfo.NoStdDepOKPkgs))
+// }
