fe412ad044c94f1de4313180851ecbf5b1e04808
shiyongbin <shiyongbin@ezbuy.com>
2017-12-12 18:37:44 +0800

    tpl

--- walkexc/tpl/init.go
+++ walkexc/tpl/init.go
@@ -14,50 +14,10 @@ var (
 		Short: "init render template",
 		Long:  ``,
 		Run: func(cmd *cobra.Command, args []string) {
-			InitExcute()
+			if err := InitExcute(); goutils.CheckErr(err) {
+			}
 		},
 	}
-
-	tpl = `{{range $k,$v := .Map}}
-	echo hello, {{$k}}:{{$v}}
-	{{range $v}}
-		echo name: {{.First}} {{.Second}}
-	{{end}}
-{{end}}
-
-{{range $i,$v := .Arr}}
-	echo arr[{{$i}}]:{{$v}}
-{{end}}
-
-{{with .Arr}}
-	echo arrs: {{join . ","}}___
-{{end}}
-
-{{loop 1 10 1}}
-{{loop 1 10 2}}
-{{loop 1 10 3}}
-
-{{range $i:=loop 1 10 2}}
-	{{$i}}th
-{{end}}
-`
-
-	data = `map:
-  Names: 
-    - 
-      First: Simth
-      Second: John
-    - 
-      First: Simth
-      Second: John2
-    - 
-      First: Simth
-      Second: John3
-arr:
-  - 1
-  - 2
-  - 3
-`
 )
 
 func init() {
@@ -66,6 +26,7 @@ func init() {
 }
 
 func InitExcute() error {
-	goutils.WriteFile(filepath.Join(viper.GetString("output"), "tpl.tpl"), goutils.ToByte(tpl))
-	return goutils.WriteFile(filepath.Join(viper.GetString("output"), "data.yaml"), goutils.ToByte(data))
+	if err := goutils.SafeWriteFile(filepath.Join(viper.GetString("output"), "tpl.tpl"), MustAsset("tpl/tpl.tpl")); goutils.CheckErr(err) {
+	}
+	return goutils.SafeWriteFile(filepath.Join(viper.GetString("output"), "data.yaml"), MustAsset("tpl/data.yaml"))
 }
